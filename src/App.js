91c85bef42c3e00166621ef9b8cf674e0b389028
Bernard <rayosobernard@gmail.com>
2020-02-27 12:11:27 +0800

    Add a header functionality
    Add a header functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './pages/homepage/Homepage';
 import ShopPage from './pages/shoppage/ShopPage';
+import Header from './components/header/Header';
 
 
 function App() {
   return (
-  <div> 
+  <div>
+    <Header/>
     <Switch>
       <Route exact path="/" component={Homepage}/>
       <Route exact path="/shop" component={ShopPage}/>
